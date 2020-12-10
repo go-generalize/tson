6e8ae8aae3ad41181ca327dda1b4d0e1044778c8
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2020-12-10 19:22:51 +0900

    Add unit tests

---
+++ decode_test.go
@@ -0,0 +1,64 @@
+package tson_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/cia-rana/tson"
+)
+
+type Foo struct {
+	T *time.Time
+}
+
+func TestUnmarshal(t *testing.T) {
+	type args struct {
+		jsonBytes []byte
+		expected  *time.Time
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "null",
+			args: args{
+				jsonBytes: []byte(`{"T": null}`),
+				expected:  nil,
+			},
+		},
+		{
+			name: "empty",
+			args: args{
+				jsonBytes: []byte(`{}`),
+				expected:  nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var v Foo
+
+			if err := tson.Unmarshal(tt.args.jsonBytes, &v); (err != nil) != tt.wantErr {
+				t.Errorf("Unmarshal() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if v.T == nil {
+				if tt.args.expected == nil {
+					return
+				}
+
+				t.Fatalf("time was nil(expected: %v)", tt.args.expected)
+			}
+
+			if tt.args.expected == nil {
+				t.Fatalf("time was not nil(actual: %v)", v.T)
+			}
+
+			if !tt.args.expected.Equal(*v.T) {
+				t.Fatalf("time mismatched(want: %v, got: %v)", tt.args.expected, v.T)
+			}
+		})
+	}
+}
